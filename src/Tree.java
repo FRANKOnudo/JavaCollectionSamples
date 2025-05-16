35808f1525196daf881a2c3f4638108bb44a77d9
FRANKOnudo <111398019+FRANKOnudo@users.noreply.github.com>
2025-05-16 08:46:03 +0000

    si

---
+++ src/Tree.java
@@ -0,0 +1,13 @@
+public class Tree<T extends Comparable<T>> {
+    protected Treenode<T> root;
+
+    public Tree() {
+        root = null;
+    }
+
+    public void add(T newvalue){
+
+        
+    }
+    
+}
